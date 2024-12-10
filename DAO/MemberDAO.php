618c4ceebfce6d33cfd0abbeb312dd4b227f86f6
辻井央輔 <23jn0339@jec.ac.jp>
2024-12-10 11:18:08 +0900

    MemberDAO（仮）

---
+++ DAO/MemberDAO.php
@@ -0,0 +1,38 @@
+<?php require_once 'DAO.php';
+
+class Member{
+   
+    public string $memberid;
+   
+    public string $password;
+}
+class MemberDAO{
+    public function get_member(string $memberid,string $password){
+        $dbh=DAO::get_db_connect();
+        $sql="select * from member where memberid=:memberid";
+    $stmt = $dbh->prepare($sql); 
+    $stmt->bindValue(':memberid',$memberid,PDO::PARAM_STR);
+    $stmt->execute(); 
+$member=$stmt->fetchObject('Member');
+if($member!== false){
+    if(password_verify($password,$member->password)){
+        return $member;
+    }
+}
+return false;
+}
+public function insert(Member $member){
+    $dbh=DAO::get_db_connect();
+    $sql="insert into Member (memberid,password)
+    values(:memberid,:password)";
+    $stmt = $dbh->prepare($sql); 
+
+  $password=password_hash($member->password,PASSWORD_DEFAULT);
+
+    
+    $stmt->bindValue(':memberid',$member->memberid,PDO::PARAM_STR);
+    $stmt->bindValue(':password',$password,PDO::PARAM_STR);
+    $stmt->execute(); 
+}
+
+}
