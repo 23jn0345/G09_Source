029c174ecba1a70db0ff816cf25646e547fcf881
深澤 <>
2024-12-17 09:34:56 +0900

    管理者ログイン修正

--- DAO/adminDAO.php
+++ DAO/adminDAO.php
@@ -2,21 +2,21 @@
 
 class Member{
    
-    public string $adminid;
+    public string $managerid;
    
     public string $password;
 }
 class MemberDAO{
-    public function get_admin(string $adminid,string $password){
+    public function get_member(string $managerid,string $password){
         $dbh=DAO::get_db_connect();
-        $sql="select * from admin where adminid=:adminid";
+        $sql="select * from administrator where managerid=:managerid";
     $stmt = $dbh->prepare($sql); 
-    $stmt->bindValue(':adminid',$adminid,PDO::PARAM_STR);
+    $stmt->bindValue(':managerid',$managerid,PDO::PARAM_STR);
     $stmt->execute(); 
-$admin=$stmt->fetchObject('Admin');
-if($admin!== false){
-    if(password_verify($password,$admin->password)){
-        return $admin;
+$manager=$stmt->fetchObject('manager');
+if($member!== false){
+    if(password_verify($password,$manager->password)){
+        return $manager;
     }
 }
 return false;
