004ae39f24ddccf7103a550f3f6a3c9518d84105
深澤 <>
2024-12-19 16:44:36 +0900

    adminDAO完成

--- DAO/adminDAO.php
+++ DAO/adminDAO.php
@@ -1,26 +1,26 @@
 <?php require_once 'DAO.php';
 
-#[\AllowDynamicProperties]
-class Manager{
+class Member{
+   
     public string $managerid;
+   
     public string $password;
 }
-#[\AllowDynamicProperties]
-class AdminDAO{
-    public function get_admin(string $managerid,string $password){
-        
+class MemberDAO{
+    public function get_member(string $managerid,string $password){
         $dbh=DAO::get_db_connect();
-        $sql= "SELECT * from administrator where managerid = :managerid";
-        $stmt = $dbh->prepare($sql); 
-        $stmt->bindValue(':managerid',$managerid,PDO::PARAM_STR);
-        $stmt->execute(); 
-
-        $manager = $stmt->fetchObject('Manager');
-        if($manager!== false){
-                return $manager;
-        }
-        return false;
+        $sql="select * from administrator where managerid=:managerid";
+    $stmt = $dbh->prepare($sql); 
+    $stmt->bindValue(':managerid',$managerid,PDO::PARAM_STR);
+    $stmt->execute(); 
+$manager=$stmt->fetchObject('manager');
+if($member!== false){
+    if(password_verify($password,$manager->password)){
+        return $manager;
     }
+}
+return false;
+}
 
 
 }
