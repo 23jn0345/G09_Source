0b01329ce88c317854783a6011437d36b31c2d76
深澤 <>
2025-01-22 16:40:17 +0900

    subscRegistration作成途中

--- DAO/config.php
+++ DAO/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1\sotsu;database=23jn03_G09');
+    define('DSN', 'sqlsrv:server=10.32.97.1\SOTSU;database=23jn03_G09');
     define('DB_USER', '23jn03_G09');
     define('DB_PASSWORD', '23jn03_G09');
--- subscRegistration.php
+++ subscRegistration.php
@@ -1,4 +1,27 @@
 <?php
+    require_once './DAO/subscRegistrationDAO.php';
+
+    $subscRegiDAO = new subscRegiDAO;
+    $freeTime_list = $subscRegiDAO->get_freetime();
+    $interval_list = $subscRegiDAO->get_interval();
+
+    // if (isset($_POST['upload'])) {//送信ボタンが押された場合
+	// 	$image = uniqid(mt_rand(), true);//ファイル名をユニーク化
+	// 	$image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);//アップロードされたファイルの拡張子を取得
+	// 	$file = "images/$image";
+    //     $sql = "INSERT INTO images(name) VALUES (:image)";
+	// 	$stmt = $dbh->prepare($sql);
+	// 	$stmt->bindValue(':image', $image, PDO::PARAM_STR);
+	// 	if (!empty($_FILES['image']['name'])) {//ファイルが選択されていれば$imageにファイル名を代入
+	// 		move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);//imagesディレクトリにファイル保存
+	// 		if (exif_imagetype($file)) {//画像ファイルかのチェック
+    //             $message = '画像をアップロードしました';
+    //             $stmt->execute();
+	// 		} else {
+	// 			$message = '画像ファイルではありません';
+	// 		}
+	// 	}
+	// }
 
 ?>
 
@@ -19,13 +42,15 @@
         <div class="title">
             <h1>サブスク詳細登録</h1>
         </div>
-   
-    <img src="netflix.png"><br>
 
-    <form>
+    <form method = "POST">
         <div class="name">
+        <p>アップロード画像</p>
+        <input type="file" name="image">
+        
+        
             <p>サブスク名<br>
-                <input type="text" name="ID" size="50px">
+                <input type="text" name="ID" size="50px" required>
             </p>
             <p>略称<br>
                 <input type="text" name="password" size="50px">
@@ -36,89 +61,50 @@
             <br>
         </div>
         <div class="category">
-            <input type="checkbox" id="cate" name="1" checked />
+            <input type="checkbox" id="cate" name="50001" checked />
             <label for="cate">動画配信</label>
-            <input type="checkbox" id="cate" name="2" />
+            <input type="checkbox" id="cate" name="50002" />
             <label for="cate">音楽配信</label>
-            <input type="checkbox" id="cate" name="3" />
+            <input type="checkbox" id="cate" name="50003" />
             <label for="cate">書籍</label>
-            <input type="checkbox" id="cate" name="4" />
+            <input type="checkbox" id="cate" name="50004" />
             <label for="cate">食品</label>
         </div>
         <br>
         <div class="payterm">
-            <p>支払い１
-                <select name="pay1">
-                    <option value="1m" selected>１カ月</option>
-                    <option value="harf">半年</option>
-                    <option value="1y">１年</option>
+        <?php for ($i = 1; $i <= 3; $i++): ?> 
+            <p>支払い<?= $i ?>
+                <select name="pay<?= $i ?>">
+                <?php foreach($interval_list as $interval): ?>
+                        <option value=""><?= $interval->date ?>日</option>
+                <?php endforeach ?>
                 </select>
-                料金１
-                <select name="amount1">
-                    <option value="500" selected>500</option>
-                    <option value="3000">3000</option>
-                    <option value="6000">6000</option>
-                </select>
+                料金<?= $i ?>
+                <?php if($i==1): ?>
+                    <input type="text" name="amount1" size="30px" required>
+                <?php else :?>
+                    <input type="text" name="amount<?= $i ?>" size="30px" required>
+                <?php endif ?>
                 無料期間
-                <select name="free1">
-                    <option value="1w" selected>１週間</option>
-                    <option value="1m">１カ月</option>
-                    <option value="3m">３カ月</option>
+                <select name="free<?= $i ?>">
+                <?php foreach($freeTime_list as $freeTime): ?>
+                    <option value=""><?= $freeTime->date ?>日</option>
+                <?php endforeach ?>
 
                 </select>
             </p>
             <br>
-            <p>支払い2
-                <select name="pay2">
-                    <option value="1m">１カ月</option>
-                    <option value="harf">半年</option>
-                    <option value="1y">１年</option>
-                </select>
-                料金2
-                <select name="amount2">
-                    <option value="500">500</option>
-                    <option value="3000">3000</option>
-                    <option value="6000">6000</option>
-                </select>
-                無料期間
-                <select name="free2">
-                    <option value="1w">１週間</option>
-                    <option value="1m">１カ月</option>
-                    <option value="3m">３カ月</option>
-
-                </select>
-            </p>
-            <br>
-            <p>支払い3
-                <select name="pay3">
-                    <option value="1m">１カ月</option>
-                    <option value="harf">半年</option>
-                    <option value="1y">１年</option>
-                </select>
-                料金3
-                <select name="amount3">
-                    <option value="500">500</option>
-                    <option value="3000">3000</option>
-                    <option value="6000">6000</option>
-                </select>
-                無料期間
-                <select name="free3">
-                    <option value="1w">１週間</option>
-                    <option value="1m">１カ月</option>
-                    <option value="3m">３カ月</option>
-
-                </select>
-            </p>
-            <br>
+            <?php endfor ?>
+            
         </div>
 
         <br>
         <p class="text">説明　　　　　<input type="text" class="detail" size="55px" required></p>
         <p>公式サイトURL<input type="text" size="55px" required></p>
         <br>
+        <input type="submit" value="検索">
     </form>
-    <button onclick="location.href='regiConfirmation.html'">確認画面へ</button>
-
+    
 
 
 
