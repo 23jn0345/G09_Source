6800d0852b05ad70a56c1e4bc36bb8e4eb9642ec
山川源太 <23jn0345@jec.acjp>
2025-02-19 09:52:43 +0900

    subscDAO.php修正

--- DAO/subscDAO.php
+++ DAO/subscDAO.php
@@ -4,20 +4,33 @@ require_once 'memberDAO.php';
 $memberDAO= new memberDAO();
 #[\AllowDynamicProperties]
 class subsc{
-    public  string $subName;
+    public string $subname;
     public string $setumei;
     public string $aliasName;
     public string $shortName;
     public string $image;
     public string $URL;
+    public int $subID;
+    public int $price;
+    public int $date;
     
     
 }
 #[\AllowDynamicProperties]
 class subscDAO { 
-   
+    public function search_subsc(string $subscName) {
+       
+        $dbh = DAO::get_db_connect();
+        
+         $sql = "SELECT subName,setumei,aliasName,shortName,image,URL FROM subsc WHERE SubName LIKE :subscName";
+         $stmt = $dbh->prepare($sql); 
+         $stmt->bindValue(':subscName','%'.$subscName.'%',PDO::PARAM_STR);
+         $stmt->execute(); 
+         $data = $stmt->fetchAll(); 
+         return $data; 
          
-
+}
+         
 public function add_favorite(int $ID,int $subID){
     $dbh = DAO::get_db_connect();
     $sql="INSERT INTO favorite (ID, subID) VALUES (:ID, :subID)";
@@ -86,8 +99,6 @@ public function get_subsc_by_keyword(string $keyword){
 				LEFT OUTER JOIN kikan as freetime ON subscplan.FreeTimeID = freetime.KikanID
 				LEFT OUTER JOIN kikan as interval ON subscplan.IntervalID = interval.KikanID
            where subname LIKE :subname OR aliasName LIKE :aliasName OR shortName LIKE :shortName OR genreName LIKE :genreName";
-
-    
     $stmt = $dbh->prepare($sql);
 
     $stmt->bindValue(':aliasName',$keyword ,PDO::PARAM_STR);
@@ -103,6 +114,90 @@ public function get_subsc_by_keyword(string $keyword){
     }
     return $data;
 }
+
+public function get_subsc_by_genre(string $genres){
+
+    $dbh = DAO::get_db_connect();
+
+    $sql = "SELECT subname,image,price,interval.date,genreName,subsc.subID
+                FROM subsc
+                LEFT OUTER JOIN genre ON subsc.GenreID = genre.GenreID
+                LEFT OUTER JOIN subscplan ON subsc.SubID = subscplan.subID
+                LEFT OUTER JOIN kikan as freetime ON subscplan.FreeTimeID = freetime.KikanID
+                LEFT OUTER JOIN kikan as interval ON subscplan.IntervalID = interval.KikanID
+                Where subsc.genreID = :genre";
+    $stmt = $dbh->prepare($sql);
+
+    $stmt->bindValue(':genre',$genres ,PDO::PARAM_STR);
+
+    $stmt->execute();
+
+    $data = [];
+    while($row=$stmt->fetchObject('subsc')){
+        $data[] =$row;
+    }
+    return $data;
+}
+
+public function get_subsc_by_keywordgenre(string $keyword,string $genres){
+
+    $dbh = DAO::get_db_connect();
+
+    $keyword = "%".$keyword."%";
+    $sql = "SELECT subname,image,price,interval.date,genreName,subsc.subID
+                FROM subsc
+                LEFT OUTER JOIN genre ON subsc.GenreID = genre.GenreID
+                LEFT OUTER JOIN subscplan ON subsc.SubID = subscplan.subID
+                LEFT OUTER JOIN kikan as freetime ON subscplan.FreeTimeID = freetime.KikanID
+                LEFT OUTER JOIN kikan as interval ON subscplan.IntervalID = interval.KikanID
+                Where (subname LIKE :subname OR aliasName LIKE :aliasName OR shortName LIKE :shortName OR genreName LIKE :genreName) AND subsc.genreID = :genre";
+    $stmt = $dbh->prepare($sql);
+
+    $stmt->bindValue(':aliasName',$keyword ,PDO::PARAM_STR);
+    $stmt->bindValue(':subname',$keyword ,PDO::PARAM_STR);
+    $stmt->bindValue(':shortName',$keyword ,PDO::PARAM_STR);
+    $stmt->bindValue(':genreName',$keyword ,PDO::PARAM_STR);
+    $stmt->bindValue(':genre',$genres ,PDO::PARAM_STR);
+
+    $stmt->execute();
+
+    $data = [];
+    while($row=$stmt->fetchObject('subsc')){
+        $data[] =$row;
+    }
+    return $data;
+}
+
+public function get_all_subsc(){
+    // データベース接続を取得
+    $dbh = DAO::get_db_connect();
+
+
+
+    // ベースSQL
+    $sql = "
+        SELECT 
+        subsc.subID, -- SubID を追加
+        subsc.subname, 
+        subsc.image, 
+        subscplan.price, 
+        kikan.date
+    FROM 
+        subsc
+    INNER JOIN subscplan ON subsc.SubID = subscplan.SubID
+    INNER JOIN kikan ON subscplan.IntervalID = kikan.KikanID
+    WHERE 1=1
+    ";
+
+    $params = [];
+    
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($params);
+    while($row=$stmt->fetchObject('subsc')){
+        $data[] =$row;
+    }
+        return $data;
+    }
 
 }
     
